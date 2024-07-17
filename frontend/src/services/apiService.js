2a7657dfd7106a18794caa853292fbb69c7a312f
Cristian <138404356+cdanni91@users.noreply.github.com>
2024-07-17 13:33:19 -0300

    Update apiService.js

--- frontend/src/services/apiService.js
+++ frontend/src/services/apiService.js
@@ -1,6 +1,14 @@
 import axios from 'axios';
 
+// Definimos la URL donde se va a ejecutar el backend de flask
+
 const API_URL = 'http://localhost:5000';
+
+
+
+// Aca se definen las funciones de que interactuan con la API de Flask en el backend
+
+
 
 export const register = (email, password) => {
     return axios.post(`${API_URL}/register`, { email, password });
